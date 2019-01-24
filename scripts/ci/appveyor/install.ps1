a7203abe428b2ad3519a424edbe02733de153d6b
Julia Signell <jsignell@gmail.com>
2019-01-24 17:26:23 -0500

    Fixing syntax

--- scripts/ci/appveyor/install.ps1
+++ scripts/ci/appveyor/install.ps1
@@ -3,7 +3,7 @@ function install() {
     conda config --set always_yes yes
     conda config --add channels conda-forge
     conda config --get channels
-    conda config –set verbosity 3
+    conda config --set verbosity 3
     conda create -n intake python=3.6 conda-build jinja2 pyyaml pytest $(python scripts/deps.py)
 }
 
