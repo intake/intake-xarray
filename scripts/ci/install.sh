a3bf45092fa7f276b90bf93c44bfc3c34950150b
Julia Signell <jsignell@gmail.com>
2019-01-24 17:33:49 -0500

    verbosity was too high

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -24,7 +24,7 @@ conda config --set auto_update_conda off
 conda config --set always_yes yes
 conda config --add channels conda-forge
 conda config --get channels
-conda config --set verbosity 3
+conda config --set verbosity 1
 
 echo "Installing dependencies."
 conda create -n intake python=3.6 ${CONDA_REQS} `python scripts/deps.py`
