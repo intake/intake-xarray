b8510df2e53a7e96f3914080cd7d1775a96be098
Martin Durant <martin.durant@utoronto.ca>
2020-03-23 08:40:04 -0400

    with dev versions

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -4,6 +4,9 @@ set -e # exit on error
 echo "Creating test env"
 conda env create -n test_env --file ci/environment-${CONDA_ENV}.yml
 source activate test_env
+# dev versions
+pip install git+https://github.com/intake/filesystem_spec --no-deps
+pip install git+https://github.com/intake/intake --no-deps
 conda list
 
 echo "Installing intake_xarray."
