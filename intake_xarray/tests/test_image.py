f0755f6468a78f10f06d6dfc91b06dfeb149e212
Aaron Spring <aaronspring@users.noreply.github.com>
2022-06-14 20:19:53 +0200

    add test

--- intake_xarray/tests/test_image.py
+++ intake_xarray/tests/test_image.py
@@ -184,3 +184,10 @@ def test_read_images_and_exif_as_glob_with_coerce():
     ds = source.read()
     assert ds['raster'].shape == (3, 256, 256, 3)
     assert ds['EXIF Image ImageWidth'].shape == (3,)
+
+
+def test_read_images_and_persist():
+    pytest.importorskip('skimage')
+    urlpath = os.path.join(here, 'data', 'images', '*')
+    source = ImageSource(urlpath=urlpath, coerce_shape=(256, 256))
+    source.export('./exported')
